35a5d5c4a9d4f36d14ed0c482dce0fad6ca1d6cf
AP Java Period 6 <apjavaperiod6@mac_unknown-nxhbraa00311315a6b7611-.78b505.nycboe.org>
2022-10-14 12:39:00 -0400

    Updated Comments

--- 06 - username/UserName.java
+++ 06 - username/UserName.java
@@ -1,17 +1,19 @@
 
 /**
- * Write a description of class Username here.
+ * Creates an Email for Students & Teachers w/First Name, Last Name, & Fav Number.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Volodymyr Pasichnyak
+ * 10/14/2022
  */
-import java.util.Scanner;
+
+import java.util.Scanner; // Import Java Scanner
 
 public class UserName {
   public static void main(String[] args) {
-    // create Scanner object
+    // Create Scanner Object
     Scanner s = new Scanner(System.in);
 
+    //Asks for First Name, Last Name, and Favorite Number
     System.out.print("Are you a Student or Teacher? ");
     String choice = s.nextLine();
     System.out.print("Enter First Name: ");
@@ -20,29 +22,37 @@ public class UserName {
     String lastName = s.nextLine();
     System.out.print("Enter Favorite Number: ");
     int favNum = s.nextInt();
-    
-    if (choice.equals("Student")){
+        
+    if (choice.equals("Student")){ // Generate @nycstudents.net if Student
         System.out.println("Hello " +  getName(firstName) + getInitial(lastName) + favNum + "@nycstudents.net");
     }
-    else{
+    else{ // Generate @shools.nyc.gov if Teacher
         System.out.println("Hello " +  getInitial(firstName) + getName(lastName) + favNum + "@schools.nyc.gov");
     }
+
+    s.close(); //Close Scanner
     
-    s.close();
-    
-  } // end main method
+  } // End Main Method
 
   /*
-   * Name: initialize
-   * Purpose: send back the first character (inital) of a nameß
-   * Input: a name (String)
-   * Return: a single character (String)
+   * Name: getInitial
+   * Purpose: Send Back the First Character (Inital) of a Nameß
+   * Input: A Name (String)
+   * Return: A Single Character (String)
    */
   
   public static String getInitial(String n) {
     return n.substring(0, 1);
-  } // end initialize method
+  } // End getInitial Method
+
+  /*
+   * Name: getName
+   * Purpose: Replicates Full First or Last Name 
+   * Input: A Name (String)
+   * Return: A Sequence of Characters (String)
+   */
+
   public static String getName(String n) {
     return n.substring(0);
-  } // end initialize method
-} // end class
+  } // End getName Method
+} // End Class

4e4cf6640f324a5c7fc52193e1e12a8af7594e54
AP Java Period 6 <apjavaperiod6@ac1mwk18gjc9h.78b505.nycboe.org>
2022-10-21 13:06:41 -0400

    Fixed Errors

--- 06-password/Password.java
+++ 06-password/Password.java
@@ -1,57 +1,62 @@
-import java.util.Scanner;
+/**
+ * Creates a Randomized Password w/Upper & Lower Case Characters & Symbols.
+ *
+ * Volodymyr Pasichnyak
+ * 10/20/2022
+ */
+
+import java.util.Scanner; // Imports Java Scanner
 
 public class Password {
   public static void main(String[] args) {
-    // create Scanner object
+    // Create Scanner Object
     Scanner s = new Scanner(System.in);
 
-    // get String input from user
-    System.out.print("Enter length of password: ");
+    // Obtain String Input from User
+    System.out.print("Enter Length of Password: ");
     int length = s.nextInt();
 
-    System.out.println("Your password is: " + generatePassword(length));
-    s.close();
-  } // end main method
+    System.out.println("Password: " + generatePassword(length));
+    s.close(); // Closes Scanner
+  } // End Main Method
   /*
    * Name: generatePassword
-   * Purpose: generate a random password
-   * Input: length of password (int)
-   * Return: the password (String)
+   * Purpose: Generate a Random Password for the User.
+   * Input: Length of a Password (int)
+   * Return: The Password (String)
    */
   public static String generatePassword(int length) {
     String password = "";
 
-    // loop 'length' times
+    // Loops 'Length' Times
     for (int i = 0; i < length; i++){
-      // generate a random number
-      // from 65 - 90 CAPITAL letters
+      // Generates a Random Number for 65-90 (CAPITAL Letters)
       int max = 90;
       int min = 65;
       int rand = (int)(Math.random()*(max - min + 1) + min);
       char c = (char)rand;
-      // from 97 - 122 lowercase
+      
+      // Generates a Random Number for 97-122 (Lowercase Letters)
       
       max = 122;
       min = 97;
       rand = (int)(Math.random()*(max - min + 1) + min);
       char a = (char)rand;
       
-      // from 33 - 47 for special characters
+      // Generates a Random Number for 33-47 (Special Characters)
       
       max = 47;
       min = 33;
       rand = (int)(Math.random()*(max - min + 1) + min);
       char b = (char)rand;
       
-      // add the char to password
-      password += c; //password = password + c;
-      password += b; //password = password + b;
-      password += a; //password = password + a;
-      System.out.println(password); 
+      // Add Characters to Password
+      password += c; //password = password + c
+      password += b; //password = password + b
+      password += a; //password = password + a
     }
     
-    return password;
+    return password; // Returns Output to the User
     
   } // end generatePassword method
-
-} // end class
+} // End Class
--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -16,18 +16,21 @@ public class UserName {
     //Asks for First Name, Last Name, and Favorite Number
     System.out.print("Are you a Student or Teacher? ");
     String choice = s.nextLine();
+    String decision1 = choice.toLowerCase();
     System.out.print("Enter First Name: ");
     String firstName = s.nextLine();
+    String decision2 = firstName.toLowerCase();
     System.out.print("Enter Last Name: ");
     String lastName = s.nextLine();
+    String decision3 = lastName.toLowerCase();
     System.out.print("Enter Favorite Number: ");
     int favNum = s.nextInt();
         
-    if (choice.equals("Student")){ // Generate @nycstudents.net if Student
-        System.out.println("Hello " +  getName(firstName) + getInitial(lastName) + favNum + "@nycstudents.net");
+    if (decision1.equals("student")){ // Generate @nycstudents.net if Student
+        System.out.println("Hello " +  getName(decision2) + getInitial(decision3) + favNum + "@nycstudents.net");
     }
     else{ // Generate @shools.nyc.gov if Teacher
-        System.out.println("Hello " +  getInitial(firstName) + getName(lastName) + favNum + "@schools.nyc.gov");
+        System.out.println("Hello " +  getInitial(decision2) + getName(decision3) + favNum + "@schools.nyc.gov");
     }
 
     s.close(); //Close Scanner
@@ -55,26 +58,4 @@ public class UserName {
   public static String getName(String n) {
     return n.substring(0);
   } // End getName Method
-  
-  public static String generatePassword(int length) {
-    String password;
-
-    
-    // loop 'length' times
-    
-    // generate a random number
-    // from 65 - 90 CAPITAL letters
-    // from 97 - 122 lowercase
-    // from 33 - 47 for special characters
-
-    int max = 90;
-    int min = 65;
-    int rand = (int)(Math.random() * (max - min + 1) + min);
-    char c = (char) rand;
-    
-    System.out.println(rand + " " + c);
-    // convert the random int to char, ex: c =(char)i;
-
-    // add the char to password
-  } // end generatePassword method
 } // End Class
